619d021ecf0e772d0af7dc54fa88ef57ed83bcdc
aksh7272 <125992439+aksh7272@users.noreply.github.com>
2023-02-27 14:33:10 +0530

    Update Java.java

--- src/Practice/Java.java
+++ src/Practice/Java.java
@@ -4,6 +4,10 @@ public class Java {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		
+		System.out.println("Hello Java");
+		
 
 	}
 
